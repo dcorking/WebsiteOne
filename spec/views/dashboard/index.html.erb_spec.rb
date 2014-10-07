f4e3aeaf3bc4e8f1c021b3c330f23dde9d2fbec5
Sasha Cooper <jinksy@gmail.com>
2014-10-07 18:41:08 +0100

    added a couple of tests to index.html.erb_spec.rb, and made them suspiciously pass withsuperficial changes to the view. Suspect this implies existing tests are inadequate?

--- spec/views/dashboard/index.html.erb_spec.rb
+++ spec/views/dashboard/index.html.erb_spec.rb
@@ -5,7 +5,9 @@ describe 'dashboard/index.html.erb', type: :view do
     assign(:stats, {articles:{count:10},
                     projects:{count:5},
                     members:{count:100},
-                    documents:{count:40}})
+                    documents:{count:40},
+                    pairing_minutes:{count:300},
+                    scrum_minutes:{count:200}})
   end
 
   it 'displays a tab view' do
@@ -19,5 +21,7 @@ describe 'dashboard/index.html.erb', type: :view do
     expect(rendered).to have_content('5 Active Projects')
     expect(rendered).to have_content('100 AgileVentures Members')
     expect(rendered).to have_content('40 Documents Created')
+    expect(rendered).to have_content('300 Pair Programming Minutes')
+    expect(rendered).to have_content('200 Scrum Minutes')
   end
 end
